10903a6459b4ca3b66cb6dbaca5db6579b5d2e6b
saworz <michal.czechowicz7@gmail.com>
2023-01-29 21:47:52 +0100

    Read files from lambda

--- src/front/streamlit_handling.py
+++ src/front/streamlit_handling.py
@@ -1,12 +1,15 @@
 from features.s3_sender import S3SENDER
 from features.remove_bg import remove_bg
 
+import PIL
 from PIL import Image
 from pathlib import Path
 from io import BytesIO
 
 import os
 import streamlit as st
+import boto3
+import json
 
 def save_uploadedfile(data_path: Path, uploadedfile: st.file_uploader):
 
@@ -20,10 +23,19 @@ def save_uploadedfile(data_path: Path, uploadedfile: st.file_uploader):
 def streamlit_handling(temp_path: Path, net):
 
     sender = S3SENDER('mihu', 'background-remover-swz')
+    lambda_client = boto3.client('lambda')
     bg_removed=False
 
     inputs_dir = temp_path / "inputs"
     st.title('Background remover')
+
+    if st.button('Read data from S3'):
+        response = lambda_client.invoke(FunctionName='crop_images', 
+                     InvocationType='RequestResponse')
+        json_string = response['Payload'].read().decode()
+        files_list = json.loads(json_string)["body"]
+        st.session_state.files_list = files_list
+        
     st.text('Upload an image and get .png without the background')
 
     uploaded_file = st.file_uploader('Upload file', type=["jpg", "jpeg", "png"])
@@ -48,7 +60,6 @@ def streamlit_handling(temp_path: Path, net):
         result_img = Image.open(result_path)
 
         st.image([mask_img, result_img], width=350, caption=["Generated mask", "Final result"])
-        st.text(str(temp_path) + "/masks/" + filename + ".png")
 
         st.session_state.mask_path = mask_path
         st.session_state.result_path = result_path
@@ -57,3 +68,5 @@ def streamlit_handling(temp_path: Path, net):
         sender.send_image(inputs_dir / uploaded_file.name, uploaded_file.name)
         sender.send_image(st.session_state.mask_path, filename + "_mask.png")
         sender.send_image(st.session_state.result_path, filename + "_result.png")
+        show_file = st.empty()
+        st.text("Files successfully uploaded to S3 bucket!")
